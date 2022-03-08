e46faf4113e219eb3caad5fa494f2c5506df2689
pankesh18 <wadekar.pankesh@gmail.com>
2022-03-08 20:24:18 +0000

    Update

---
+++ Library_Management/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bookworm.wsgi --log-file -
--- Library_Management/library_management/library_management/settings.py
+++ Library_Management/library_management/library_management/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3kaslx@1dda(t%cy0o)_fu1ij2%&^)v7&+tcw1rfik@7zdq_mz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['a2zlibrary.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -49,12 +49,16 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'library_management.urls'
